4a4e5237dd12d842f1cc3f644c934377a500104a
Nico Vega <91123530+MrGG14@users.noreply.github.com>
2024-03-04 13:29:15 +0100

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,6 +1,15 @@
 from grobid_client.grobid_client import GrobidClient
 
+import os
+import xml.etree.ElementTree as ET
+from grobid_client.grobid_client import GrobidClient
+from utils import utils
 
-if __name__ == "__main__":
-    client = GrobidClient(config_path="./config.json")
-    client.process("processFulltextDocument", "./resources/test_pdf", output="./resources/test_out/", consolidate_citations=True, tei_coordinates=True, force=True)
+# Directorio donde se encuentran los archivos XML
+directorio = './output/papers'
+numero_figuras_paper = []
+listado_archivos = []
+
+client = GrobidClient(config_path="./config.json")
+client.process("processFulltextDocument", ".", output="./output/", consolidate_citations=True, tei_coordinates=True, n=20)
+#if __name__ == "__main__":
